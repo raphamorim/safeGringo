d89cd97d04a6889f9b8ad154acc651332cf9640d
Hugo Roque <hugolnx@gmail.com>
2015-12-12 10:29:49 -0200

    creating scripts folder

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 var Place = require('./place');
 
-require('mongoose').connect('mongodb://localhost/test');
+//require('mongoose').connect('mongodb://localhost/test');
 
-Place.search("Forte de copacabana", function(err, places) {
-  console.log(places);
-});
+//Place.search("Forte de copacabana", function(err, places) {
+//  console.log(places);
+//});
 //place.save(function(err) {
 //  if(err) {
 //    console.log(err);
