c943c577d9d3c29b9d2f4d00cbac564fd5cd33d2
Daniel Filho <daniel.acfilho@gmail.com>
2015-12-12 14:29:25 -0200

    Enable a lazy loading for the cards

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -4,12 +4,36 @@ function bootAutoComplete() {
     serviceUrl: '/api/search',
     transformResult: function(response) {
       return {
-        suggestions: $.map(JSON.parse(response).suggestions, function(place) {
-          return { value: place.name, data: place.type }
+        suggestions: $.map(JSON.parse(response).suggestions, function(suggestion) {
+          return { value: suggestion.name, data: suggestion.type, photo: suggestion.photos[0]}
         })
       };
+    },
+    onSearchComplete: function(query, suggestions) {
+      $('.card-list .card').remove();
+
+      var cards = $.map(suggestions, function(suggestion) {
+        return suggestionMarkup(suggestion.value, suggestion.data, suggestion.photo);
+      });
+
+      $('.card-list').append(cards);
     }
   });
 }
 
-bootAutoComplete();
+function suggestionMarkup(suggestionName, suggestionPoint, suggestionImageUrl) {
+  var cardContainer = '<div class="card">' +
+                      '<div class="card-category">' +
+                      '<span>' + suggestionPoint + '</span>' +
+                      '<h2>' + suggestionName + '</h2>' +
+                      '</div>' +
+                      '<div class="card-image">' +
+                      '<img src="' + suggestionImageUrl + '" alt="suggestionName">'
+                      '</div></div>';
+
+  return cardContainer;
+}
+
+$(function() {
+  bootAutoComplete();
+});
